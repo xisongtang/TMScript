57232406d25f4c53e1c2c0d6b026472e58889210
xisongtang <xisong.tang@qq.com>
2017-04-25 17:55:39 +0800

    customizer entry

---
+++ CustomizerEntry.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         customizer entry
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  add customizer entry in my design
+// @author       xishuai
+// @match        http://cabinet.qunhequnhe.com/cloud/mydesign
+// @match        http*://alpha.kujiale.com/cloud/mydesign
+// @match        http*://*.kujiale.com/mydesign
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var ul = document.querySelector('ul.deco-list');
+    for (var i = 0; i < ul.children.length; i++) {
+        var li = ul.children[i];
+        var planId = li.dataset.planid, designId = li.dataset.dsid, isDecoration = li.dataset.isdeco === "true";
+        if (!isDecoration) continue;
+        var parent = li.querySelector('div.actions');
+        var host = window.location.host;
+        var url;
+        if (host.startsWith("beta") || host.startsWith("yun")) {
+            url = "/customized?obsdesignid=" + designId;
+        } else {
+            url = "/cloud/customized?obsdesignid=" + designId;
+        }
+        var a = document.createElement("a");
+        a.href = url;
+        a.classList = "ktask-ds fl br3";
+        a.target = "_blank";
+        a.append("全屋定制");
+        parent.appendChild(a);
+    }
+})(); 
