aff2980a5a39d9b3d2c1bff26c1b62fe191dd3a7
xishuai <xishuai@qunhemail.com>
2018-03-21 17:37:32 +0800

    add diy h5 and is compatible with pcenter page

--- CustomizerEntry.user.js
+++ CustomizerEntry.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         customizer entry
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.21
 // @description  add customizer entry in my design
 // @author       xishuai
 // @match        */mydesign*
 // @match        */*/mydesign*
+// @match        */pcenter*
 // @grant        none
 // ==/UserScript==
 
@@ -23,18 +24,29 @@ setTimeout(
             var designId = regres[1];
             var huxing = btn.querySelector('.modify-huxing');
             var host = window.location.host;
-            var url;
+            var url, dUrl;
             if (host.startsWith("beta") || host.startsWith("yun")) {
                 url = "/customized?obsdesignid=";
+                dUrl = "/tool/h5/diy?designid=";
             } else {
                 url = "/cloud/customized?obsdesignid=";
+                dUrl = "/cloud/tool/h5/diy?designid=";
             }
             url += designId + "&redirecturl=" + encodeURIComponent(href);
+            dUrl += designId + "&redirecturl=" + encodeURIComponent(location.href);
+
             var node = huxing.cloneNode(true);
             node.style.zIndex = 10;
             var a = node.childNodes[0];
             a.innerHTML = "全屋定制";
             a.href = url;
+            btn.appendChild(node);
+
+            node = huxing.cloneNode(true);
+            node.style.zIndex = 10;
+            a = node.childNodes[0];
+            a.innerHTML = "DIY h5";
+            a.href = dUrl;
             btn.appendChild(node);
         }
     }, 1000);
